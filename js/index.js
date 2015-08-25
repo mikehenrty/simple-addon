f61c1da8f2ad7bfb49aff03522217dce7b8662b9
chrisdavidmills <chrisdavidmills@gmail.com>
2015-08-25 22:25:53 +0100

    creating new sample add-on, using the new WebExtensions form

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 window.addEventListener('DOMContentLoaded', function() {
-  alert('Hello World!');
+  var body = document.querySelector('body');
+  var fxosBanner = document.createElement('div');
+  fxosBanner.classList.add('fxos-banner');
+  var bannerText = document.createElement('p');
+  var closeBtn = document.createElement('button');
+
+  fxosBanner.appendChild(bannerText);
+  fxosBanner.appendChild(closeBtn);
+  body.appendChild(fxosBanner);
+
+  closeBtn.textContent = 'X';
+  bannerText.textContent = 'Wow, you have an extension installed!';
+
+  closeBtn.onclick = function() {
+  	fxosBanner.parentNode.removeChild(fxosBanner);
+  }
 });
