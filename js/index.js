dd20aeebf59fe628ae8db308753f40679251f17c
chrisdavidmills <chrisdavidmills@gmail.com>
2015-08-28 11:29:28 +0100

    adding JS code to guard against multiple injections and ensure the code is run at the right time

--- js/index.js
+++ js/index.js
@@ -1,18 +1,39 @@
-window.addEventListener('DOMContentLoaded', function() {
-  var body = document.querySelector('body');
-  var fxosBanner = document.createElement('div');
-  fxosBanner.classList.add('fxos-banner');
-  var bannerText = document.createElement('p');
-  var closeBtn = document.createElement('button');
+// If injecting into an app that was already running at the time
+// the app was enabled, simply initialize it.
+if (document.documentElement) {
+  initialize();
+}
 
-  fxosBanner.appendChild(bannerText);
-  fxosBanner.appendChild(closeBtn);
-  body.appendChild(fxosBanner);
+// Otherwise, we need to wait for the DOM to be ready before
+// starting initialization since add-ons are injected
+// *before* `document.documentElement` is defined.
+else {
+  window.addEventListener('DOMContentLoaded', initialize);
+}
 
-  closeBtn.textContent = 'X';
-  bannerText.textContent = 'Wow, you have an extension installed!';
+function initialize() {
+  if (document.documentElement.dataset.helloWorldInitialized) {
+    // Already injected, abort.
+    return;
+  } else {
+    var body = document.querySelector('body');
+    var fxosBanner = document.createElement('div');
+    fxosBanner.classList.add('fxos-banner');
+    var bannerText = document.createElement('p');
+    var closeBtn = document.createElement('button');
 
-  closeBtn.onclick = function() {
-  	fxosBanner.parentNode.removeChild(fxosBanner);
+    fxosBanner.appendChild(bannerText);
+    fxosBanner.appendChild(closeBtn);
+    body.appendChild(fxosBanner);
+
+    closeBtn.textContent = 'X';
+    bannerText.textContent = 'Wow, you have an extension installed!';
+
+    document.documentElement.dataset.helloWorldInitialized = true;
+
+    closeBtn.onclick = function() {
+    	fxosBanner.parentNode.removeChild(fxosBanner);
+      document.documentElement.dataset.helloWorldInitialized = false;
+    }
   }
-});
+}
